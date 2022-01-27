bc9acfe4f853ce520b5589c45e32701a32ff49dd
穗乃果 <1763575579@qq.com>
2022-01-27 13:57:42 +0800

    fix bug

--- Modules/Windows/ExternalMenuWindow.xaml.cs
+++ Modules/Windows/ExternalMenuWindow.xaml.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
-using GTA5OnlineTools.ViewModels;
 using GTA5OnlineTools.Common.Utils;
 using GTA5OnlineTools.Features.Core;
 using GTA5OnlineTools.Features.Data;
