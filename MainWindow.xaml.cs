98e0a7784236dd6bf7591ef4685a00c13304e218
穗乃果 <1763575579@qq.com>
2022-07-17 16:59:45 +0800

    更新部分UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -242,58 +242,6 @@ public partial class MainWindow : Window
     {
         try
         {
-            // 创建、清空缓存文件夹
-            Directory.CreateDirectory(FileUtil.Cache_Path);
-            FileUtil.DelectDir(FileUtil.Cache_Path);
-
-            // 创建指定文件夹，用于释放必要文件和更新软件（如果已存在则不会创建）
-            Directory.CreateDirectory(FileUtil.Config_Path);
-            Directory.CreateDirectory(FileUtil.Kiddion_Path);
-            Directory.CreateDirectory(FileUtil.KiddionScripts_Path);
-
-            // 释放必要文件
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "Kiddion.exe", FileUtil.Kiddion_Path + "Kiddion.exe");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "Kiddion_Chs.exe", FileUtil.Kiddion_Path + "Kiddion_Chs.exe");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "SubVersion.exe", FileUtil.Kiddion_Path + "SubVersion.exe");
-            // 释放前先判断，防止覆盖配置文件
-            if (!File.Exists(FileUtil.Kiddion_Path + "config.json"))
-                FileUtil.ExtractResFile(FileUtil.Resource_Path + "config.json", FileUtil.Kiddion_Path + "config.json");
-            if (!File.Exists(FileUtil.Kiddion_Path + "teleports.json"))
-                FileUtil.ExtractResFile(FileUtil.Resource_Path + "teleports.json", FileUtil.Kiddion_Path + "teleports.json");
-            if (!File.Exists(FileUtil.Kiddion_Path + "vehicles.json"))
-                FileUtil.ExtractResFile(FileUtil.Resource_Path + "vehicles.json", FileUtil.Kiddion_Path + "vehicles.json");
-            if (!File.Exists(FileUtil.KiddionScripts_Path + "Readme.api"))
-                FileUtil.ExtractResFile(FileUtil.Resource_Path + "scripts.Readme.api", FileUtil.KiddionScripts_Path + "Readme.api");
-            if (!File.Exists(FileUtil.Kiddion_Path + "settings.ini"))
-                FileUtil.ExtractResFile(FileUtil.Resource_Path + "settings.ini", FileUtil.Kiddion_Path + "settings.ini");
-
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "scripts.pre_skip.lua", FileUtil.KiddionScripts_Path + "pre_skip.lua");
-            //if (!File.Exists(FileUtil.KiddionScripts_Path + "scripts.sirius.lua.example"))
-            //FileUtil.ExtractResFile(FileUtil.Resource_Path + "scripts.sirius.lua.example", FileUtil.KiddionScripts_Path + "sirius.lua.example");
-
-            /*****************************************************************************************************/
-
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "GTAHax.exe", FileUtil.Cache_Path + "GTAHax.exe");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "stat.txt", FileUtil.Cache_Path + "stat.txt");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "BincoHax.exe", FileUtil.Cache_Path + "BincoHax.exe");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "LSCHax.exe", FileUtil.Cache_Path + "LSCHax.exe");
-
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "Bread.dll", FileUtil.Cache_Path + "Bread.dll");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "Bread_Chs.dll", FileUtil.Cache_Path + "Bread_Chs.dll");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "PackedStatEditor.dll", FileUtil.Cache_Path + "PackedStatEditor.dll");
-
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "DefenderControl.exe", FileUtil.Cache_Path + "DefenderControl.exe");
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "DefenderControl.ini", FileUtil.Cache_Path + "DefenderControl.ini");
-
-            FileUtil.ExtractResFile(FileUtil.Resource_Path + "MyInjectMenu.dll", FileUtil.Cache_Path + "MyInjectMenu.dll");
-        }
-        catch (Exception ex)
-        {
-            MsgBoxUtil.ExceptionMsgBox(ex);
-        }
-
-        try
-        {
             // 刷新DNS缓存
             CoreUtil.CMD_Code("ipconfig /flushdns");
             // 检查更新
